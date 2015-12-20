73f61cb972262bc6d02f87a64041665452f22268
Brian Powell <bpowell@bitmapped.net>
2015-12-19 22:45:44 -0500

    Initial commit.

---
+++ SafeFilenames/SafeFilenames/SafeFilenames.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SafeFilenames
+{
+    static public class SafeFilenames
+    {
+        /// <summary>
+        /// Gets a safe version of the string for use in a filename.
+        /// </summary>
+        /// <param name="filename">Filename to sanitize.</param>
+        /// <param name="replacement">String to use to replace invalid characters.</param>
+        /// <returns>Safe filename.</returns>
+        static public string GetSafeFilename(string filename, string replacement = "_")
+        {
+            // Solution from http://stackoverflow.com/questions/2230826/remove-invalid-disallowed-bad-characters-from-filename-or-directory-folder
+            // CC-BY-SA 3.0 license
+            return string.Join("_", filename.Split(Path.GetInvalidFileNameChars()));
+        }
+    }
+}
---
+++ SafeFilenames/SafeFilenames/SafeFilenamesExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SafeFilenames
+{
+    static public class SafeFilenamesExtension
+    {
+        /// <summary>
+        /// Gets a safe version of the string for use in a filename.
+        /// </summary>
+        /// <param name="filename">Filename to sanitize.</param>
+        /// <param name="replacement">String to use to replace invalid characters.</param>
+        /// <returns>Safe filename.</returns>
+        static public string GetSafeFilename(this string filename, string replacement = "_")
+        {
+            return SafeFilenames.GetSafeFilename(filename, replacement);
+        }
+
+    }
+}
